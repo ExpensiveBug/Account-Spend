7681fe46593b68839aa41be1aee9e83e63628248
ExpensiveBug <nidhireddu40@gmail.com>
2026-02-28 14:53:08 +0530

    updated sidebar

--- start.py
+++ start.py
@@ -1,67 +1,29 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import account, expense, income, report
 
+st.set_page_config(page_title= "Account Spend")
 
-# app Background
-st.markdown("""
-<style>
-        # remove sidebar
-        [data-testid="stSidebar"] {
-                display: none;
-            }
-        .stApp{
-            background-color : #7088f5;
-            }
-        /* Hide Streamlit Branding */
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-            
-        .stButton>button {
-        width: 100%;
-        border-radius: 14px;
-        color: white;
-        font-weight: 600;
-        padding: 12px 24px;
-        background-color : #0437F2;
-        }
-</style>""", unsafe_allow_html=True)
-
-st.markdown("<h1 style = 'text-align:center;'> $pent count</h1>",unsafe_allow_html=True)
-
-# Navigation bar
-selected = option_menu(
-    menu_title=None,
-    options=["Account","Expense","Income","Report"],
-    icons=["person","cash-stack","wallet2","bar-chart"],
-    orientation="horizontal",
-    styles={
-        "container": {
-            "background-color":"#0f0b05"
-        },
-        "nav-link":{
-            "color":"#feeded"
-        },
-        "nav-link-selected":{
-            "background-color":"#2CB58E"
-        }
-    }
-)
-# pages
-if selected =="Account":
-    import pages.account as a
-    a.show()
-
-elif selected == "Expense":
-    import pages.expense as ep
-    ep.show()
-
-elif selected == "Income":
-    import pages.income as inc
-    inc.show()
-
-elif selected == "Report":
-    import pages.report as rep
-    rep.show()
-
-
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+    
+def run():
+    with st.sidebar:
+        selected =  option_menu(menu_title=None, 
+                            options=["Account", "Expense", "Income", "Report"], 
+                            icons = ["person-circle","cash-stack","wallet2","bar-chart"], default_index=0,  
+                            styles = {
+                                "container":{"padding" : "5!important", "background-color":"black"},  
+                                "icon" : {"color":"white", "font-size":"23px"},
+                                "nav-link":{"color":"white","font-size":"20px", "text-align":"left","margin":"0px"},
+                                "nav-link-selected":{"background-color":"#02ab21"},  })
+    
+    if selected == "Account":
+        account.app()
+    elif selected == "Expense":
+        expense.app()
+    elif selected == "Income":
+        income.app()
+    elif selected == "Report":
+        report.app()
+run()
